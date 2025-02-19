b95f6b4b8c9b331477346a9505afebe3b2c3c068
Tejas <ttejas220@gmail.com>
2025-02-19 18:35:29 +0530

    fixed some errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
